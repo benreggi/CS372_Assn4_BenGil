24c70cc8664e2c5a8d445449b3822e6c446fd27f
Nick Gilbert <ngilbert@gmail.com>
2011-04-11 20:38:21 -0500

    Initial checkin.

---
+++ BenjaminGilbert-Assn4-SortProg.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Filename     : BenjaminGilbert-Assn4-SortProg.cpp
+ * Description  : 
+ * Date         : 4/24/2011
+ * Designer     : Reggi Benjamin
+ *				  Nicholas Gilbert
+ * Functions    : 
+ ******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+// global constants ////////////////////////////////////////////////////////////
+
+
+// Prototypes //////////////////////////////////////////////////////////////////
+
+
+/******************************************************************************
+ * Description  : 
+ * OUTPUT
+ *   Return     : 
+ * Calls To     : 
+ *****************************************************************************/
+int main( int argc, char* argv[] )
+{  
+  // system pause prior to program exit
+  cin.ignore( 100, '\n');
+  cout << endl << "Press ENTER to exit.";
+  cin.get();
+  cout << endl;
+  return 0;
+}
+
+/******************************************************************************
+// FUNCTION		    : 
+// DESCRIPTION    	: 
+// INPUT
+//		Parameters	:
+// OUTPUT   	
+//  	Return Val	: 
+//  	Parameters	: 
+// CALLS TO			: 
+// IMPLEMENTED BY 	: 
+/*****************************************************************************/
