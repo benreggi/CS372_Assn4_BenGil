97b1184ba52c33b96f2e9d98f235a2b56cc77353
Nick Gilbert <ngilbert@gmail.com>
2011-04-15 06:29:31 -0500

    Added method for generating the array of randon intgers...  Not sure if this will compile atm...bad bad.

--- BenjaminGilbert-Assn4-SortProg.h
+++ BenjaminGilbert-Assn4-SortProg.h
@@ -10,10 +10,23 @@
 
 using namespace std;
 
-// global constants ////////////////////////////////////////////////////////////
+// Global Constants ////////////////////////////////////////////////////////////
+const int MAX_ARRAY_SIZE = 100000;
+const int MAX_NUMBER_USER_SELECTIONS = 2;
+
+enum UserSelections
+{
+  SELECTION_BUBBLE = 0,
+  SELECTION_INSERTION,
+  SELECTION_MERGE,
+  SELECTION_QUICK,
+  SELECTION_EXIT,
+  SELECTION_COUNT
+};
 
 
 // Prototypes //////////////////////////////////////////////////////////////////
+void InitializeArrayOfRandomIntegers( int randomIntegers[] );
 
 
 /******************************************************************************
@@ -23,7 +36,12 @@ using namespace std;
  * Calls To     : 
  *****************************************************************************/
 int main( int argc, char* argv[] )
-{  
+{
+  int randomIntegers[MAX_ARRAY_SIZE];
+  UserSelections userSelections[SELECTION_COUNT];
+  
+  InitializeArrayOfRandomIntegers( randomIntegers );
+  
   // system pause prior to program exit
   cin.ignore( 100, '\n');
   cout << endl << "Press ENTER to exit.";
@@ -31,6 +49,47 @@ int main( int argc, char* argv[] )
   cout << endl;
   return 0;
 }
+
+//*********************************************************************
+// FUNCTION       : InitializeArrayOfRandomIntegers
+// DESCRIPTION    : generates an array of random numbers between 1 and 
+//			            30,000
+// INPUT
+//	   Parameters	: randomIntegers[]  int array to put numbers in
+// OUTPUT   
+//  	Parameters	: randomIntegers[]  int array to put numbers in
+// CALLS TO	      : none
+// IMPLEMENTED BY	: 
+//**********************************************************************
+void InitializeArrayOfRandomIntegers( int randomIntegers[] )
+{
+  const int RANDOM_MAX = 30000,
+            RANDOM_MIN = 1;
+  
+  // seed random number generator
+  srand( (unsigned)time(NULL) );
+  
+  // create random numbers
+  for( int iter = 0; iter < MAX_ARRAY_SIZE; ++iter )
+    listOfIntegers[iter] = rand() % RANDOM_MAX + RANDOM_MIN;
+}
+
+//*****************************************************************
+// FUNCTION		: GetUserSelections
+// DESCRIPTION	: gets user choices for sorts to run and times to 
+//				run them
+// INPUT
+//	Parameters	: maxSelections	- number of sorts to select
+// OUTPUT   
+//  	Parameters	: iterations	- number of iterations to perform
+//			  selections	- array of enumerated type Selections
+// CALLS TO		: none
+// IMPLEMENTED BY	: 
+//**********************************************************************
+void GetUserSelections( int maxSelections, int &iterations, Selections[] selections )
+{
+}
+
 
 /******************************************************************************
 // FUNCTION		    : 
