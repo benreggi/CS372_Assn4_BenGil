1468e4aab5ae390840ecfda5d62189f4fccbff5f
Nick Gilbert <ngilbert@gmail.com>
2011-04-17 21:22:41 -0500

    Began implementing Main and PerfomSort methods.

--- BenjaminGilbert-Assn4-SortProg.cpp
+++ BenjaminGilbert-Assn4-SortProg.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Filename     : BenjaminGilbert-Assn4-SortProg.cpp
+ * Filename     : GilbertReggi-Assn4-SortProg.cpp
  * Description  : 
- * Date         : 4/24/2011
+ * Date         : 4/26/2011
  * Designer     : Reggi Benjamin
  *				        Nicholas Gilbert
  * Functions    : InitializeArrayOfRandomIntegers  ( int randomIntegers[] );
@@ -20,12 +20,14 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <ctime>
 
 using namespace std;
 
-typedef float (*pt2SortFunction)(int randomIntegers[], int sortedIntegers[]);
+typedef void (*pt2SortFunction)(int unorderedIntegers[]);
 
 // Global Constants ////////////////////////////////////////////////////////////
+const int SORT_ERROR = -1;
 const int MAX_ARRAY_SIZE = 100000;
 const int MAX_NUMBER_USER_SELECTIONS = 2;
 
@@ -39,54 +41,63 @@ enum UserSelections
   SELECTION_COUNT
 };
 
-
 // Prototypes //////////////////////////////////////////////////////////////////
 void InitializeArrayOfRandomIntegers  ( int randomIntegers[] );
-void GetUserSelections                ( int maxSelections, int &iterations,
+void GetUserSelections                ( int &iterations,
                                         UserSelections selections[] );
-double PerformSort                    ( double (*pt2SortFunction)
-                                               (int randomIntegers[],
-                                                int sortedIntegers[]));
-double PerformBubbleSort              ( int unorderedIntegers[],
-                                        int sortedIntegers[]);
-double PerformInsertionSort           ( int unorderedIntegers[],
-                                        int sortedIntegers[]);
-double PerformMergeSort               ( int unorderedIntegers[],
-                                        int sortedIntegers[]);
+int PerformSort                       ( void (*pt2SortFunction)
+                                             (int randomIntegers[]) );
+void PerformBubbleSort                ( int unorderedIntegers[] );
+void PerformInsertionSort             ( int unorderedIntegers[] );
+void PerformMergeSort                 ( int unorderedIntegers[] );
 void MergeSort                        ( int unorderedList[],
                                         int lowIdx,
-                                        int highIdx);
+                                        int highIdx );
 void MergeList                        ( int unorderedList[],
                                         int lowIdx,
                                         int midIdx,
-                                        int highIdx);
-double PerformQuickSort               ( int unorderedIntegers[],
-                                        int sortedIntegers[] );
+                                        int highIdx );
+void PerformQuickSort                 ( int unorderedIntegers[] );
 void QuickSort                        ( int unorderedList[],
                                         int lowIdx,
                                         int highIdx);
 bool VerifySortedArray                ( int sortedIntegers[] );
-void DisplayResultAverage             ( double **sortTimesByType[] );
+void DisplayResultAverage             ( int **sortTimesByType );
 
 
 //*********************************************************************
-// FUNCTION		: main
-// DESCRIPTION	: calls functions to let user choose 2 types of sorts
-//				and compare the time taken to sort identical lists
-//				using both methods
+// FUNCTION       : main
+// DESCRIPTION    : calls functions to let user choose 2 types of sorts
+//                  and compare the time taken to sort identical lists
+//                  using both methods
 // INPUT
-//	Parameters	: none
+//	Parameters    : none
 // OUTPUT   	
 //  	Return Val	: 
-// CALLS TO		: GetUserSelections, PerformSort, DisplayResults
+// CALLS TO       : GetUserSelections, PerformSort, DisplayResults
 // IMPLEMENTED BY	: Nick
 //**********************************************************************
 int main( int argc, char* argv[] )
 {
-  pt2SortFunction sortFunctions[SELECTION_COUNT-1] = { NULL };
+  int iterations = 0;
   UserSelections userSelections[SELECTION_COUNT];
-  double **sortTimes = new double *[SELECTION_COUNT];
+  pt2SortFunction sortFunctions[SELECTION_COUNT-1] = { NULL };
+  sortFunctions[SELECTION_BUBBLE] = &PerformBubbleSort;
+  sortFunctions[SELECTION_INSERTION] = &PerformInsertionSort;
+  sortFunctions[SELECTION_MERGE] = &PerformMergeSort;
+  sortFunctions[SELECTION_QUICK] = &PerformQuickSort;
+  int **sortTimes = new int *[SELECTION_COUNT];
   
+  GetUserSelections( iterations, userSelections );
+  
+  for( int sortSelectionIter = 0; sortSelectionIter < 2; ++sortSelectionIter )
+  {
+    sortTimes[userSelections[sortSelectionIter]] = new int[iterations];
+    for( int iter = 0; iter < iterations; ++iter )
+      sortTimes[userSelections[sortSelectionIter]][iter] = PerformSort(sortFunctions[userSelections[sortSelectionIter]]);
+  }
+  
+  DisplayResultAverage( sortTimes );
   
   // system pause prior to program exit
   cin.ignore( 100, '\n');
@@ -97,18 +108,18 @@ int main( int argc, char* argv[] )
 }
 
 //*****************************************************************
-// FUNCTION		: GetUserSelections
-// DESCRIPTION	: gets user choices for sorts to run and times to 
-//				run them
+// FUNCTION       : GetUserSelections
+// DESCRIPTION    : gets user choices for sorts to run and times to 
+//                  run them
 // INPUT
-//	Parameters	: maxSelections	- number of sorts to select
+//	Parameters    : maxSelections	- number of sorts to select
 // OUTPUT   
 //  	Parameters	: iterations	- number of iterations to perform
-//			  selections	- array of enumerated type Selections
-// CALLS TO		: none
-// IMPLEMENTED BY	: 
+//                  selections	- array of enumerated type Selections
+// CALLS TO       : none
+// IMPLEMENTED BY : Ben
 //**********************************************************************
-void GetUserSelections( int maxSelections, int &iterations, UserSelections selections[] )
+void GetUserSelections( int &iterations, UserSelections selections[] )
 {
 }
 
@@ -121,7 +132,7 @@ void GetUserSelections( int maxSelections, int &iterations, UserSelections selec
 // OUTPUT   
 //  	Parameters	: unorderedIntegers[]  int array to put numbers in
 // CALLS TO	      : none
-// IMPLEMENTED BY	: 
+// IMPLEMENTED BY	: Nick
 //**********************************************************************
 void InitializeArrayOfRandomIntegers( int unorderedIntegers[] )
 {
@@ -137,86 +148,98 @@ void InitializeArrayOfRandomIntegers( int unorderedIntegers[] )
 }
 
 //*********************************************************************
-// FUNCTION		: PerformSort
-// DESCRIPTION	: Executes the passed in sort function and monitors
-//			  the time it takes to execute.
+// FUNCTION       : PerformSort
+// DESCRIPTION    : Executes the passed in sort function and monitors
+//                  the time it takes to execute.
 // INPUT
-//	Parameters	: float (*pt2SortFunction) 	- pointer to function
+//	Parameters    : (*pt2SortFunction)   - pointer to function
 // OUTPUT   
-//	Return	: double				- time taken to execute
-// CALLS TO		: InitializeArrayOfRandomIntegers
-//			  pt2SortFunction
-//			  VerifyOrderedArray
-// IMPLEMENTED BY	: Nick
+//	Return        : int				           - time taken to execute
+// CALLS TO       : InitializeArrayOfRandomIntegers
+//                  pt2SortFunction
+//                  VerifyOrderedArray
+// IMPLEMENTED BY : Nick
 //*********************************************************************
-double PerformSort(float (*pt2SortFunction)(int randomIntegers[], int sortedIntegers[]))
+int PerformSort(void (*pt2SortFunction)(int unorderedIntegers[]))
 {
-  int randomIntegers[MAX_ARRAY_SIZE];
-  InitializeArrayOfRandomIntegers( randomIntegers );
+  int startTime, endTime;
+  int unorderedIntegers[MAX_ARRAY_SIZE];
+  
+  InitializeArrayOfRandomIntegers( unorderedIntegers );
+  
+  startTime = clock();  
+  (*pt2SortFunction)(unorderedIntegers);    
+  endTime = clock();
+  
+  if( VerifySortedArray( unorderedIntegers ) )
+    return endTime - startTime;
+  
+  return SORT_ERROR;
 }
 
 //*********************************************************************
-// FUNCTION		: PerformBubbleSort
-// DESCRIPTION	: sorts an array of integers using the bubble sort
-//			  method
+// FUNCTION       : PerformBubbleSort
+// DESCRIPTION	  : sorts an array of integers using the bubble sort
+//                  method
 // INPUT
-//	Parameters	: unorderedIntegers[] - array of unordered integers
+//	Parameters	  : unorderedIntegers[] - array of unordered integers
 // OUTPUT   
-//  	Parameters	: sortedIntegers[] - int array of sorted numbers
-//	Return	:  double	- time taken to sort
-// CALLS TO		: 
+//  	Parameters	: unorderedIntegers[] - int array of sorted numbers
+//	Return        : void
+// CALLS TO       : 
 // IMPLEMENTED BY	: 
 //*********************************************************************
-double PerformBubbleSort(int unorderedIntegers[], int sortedIntegers[])
+void PerformBubbleSort( int unorderedIntegers[] )
 {
 }
 
 //*********************************************************************
-// FUNCTION		: PerformInsertionSort
-// DESCRIPTION	: sorts an array of integers using the insertion sort
-//			  method
+// FUNCTION       : PerformInsertionSort
+// DESCRIPTION    : sorts an array of integers using the insertion sort
+//                  method
 // INPUT
-//	Parameters	: unorderedIntegers[] - array of unordered integers
+//	Parameters    : unorderedIntegers[] - array of unordered integers
 // OUTPUT   
-//  	Parameters	: sortedIntegers[] - int array of sorted numbers
-//	Return	: double	- time taken to sort
-// CALLS TO		: 
+//  	Parameters	: unorderedIntegers[] - int array of sorted numbers
+//	Return        : void	- time taken to sort
+// CALLS TO		    : 
 // IMPLEMENTED BY	: 
 //*********************************************************************
-double PerformInsertionSort(int unorderedIntegers[], int sortedIntegers[])
+void PerformInsertionSort( int unorderedIntegers[] )
 {
 }
 
 //*********************************************************************
-// FUNCTION		: PerformMergeSort
-// DESCRIPTION	: sorts an array of integers using the merge sort
-//			  method
+// FUNCTION       : PerformMergeSort
+// DESCRIPTION    : sorts an array of integers using the merge sort
+//                  method
 // INPUT
-//	Parameters	: unorderedIntegers[] - array of unordered integers
+//	Parameters    : unorderedIntegers[] - array of unordered integers
 // OUTPUT   
-//  	Parameters	: sortedIntegers[] - array of sorted integers
-//	Return	: double	- time taken to sort
-// CALLS TO		: MergeSort
-//			  MergeList
-// IMPLEMENTED BY	: Nick
+//  	Parameters	: unorderedIntegers[] - array of sorted integers
+//	Return        : void
+// CALLS TO       : MergeSort
+//                  MergeList
+// IMPLEMENTED BY : Nick
 //*********************************************************************
-double PerformMergeSort(int unorderedIntegers[], int sortedIntegers[])
+void PerformMergeSort( int unorderedIntegers[] )
 {
+  MergeSort( unorderedIntegers, 0, MAX_ARRAY_SIZE );
 }
 
 //*********************************************************************
-// FUNCTION		: MergeSort
-// DESCRIPTION	: Recursive implementation of merge sort algorithm
+// FUNCTION       : MergeSort
+// DESCRIPTION    : Recursive implementation of merge sort algorithm
 // INPUT
-//	Parameters	: lowIdx		- low index of range in list to merge
-//			  highIdx		- high index of range “ “ “ “
+//	Parameters    : lowIdx		- low index of range in list to merge
+//                  highIdx		- high index of range “ “ “ “
 // OUTPUT   
 //  	Parameters	: unorderedList[] 	- array fully or partially ordered numbers
-//	Return	: void
-// CALLS TO		: 
-// IMPLEMENTED BY	: Nick
+//    Return      : void
+// CALLS TO       : MergeList 
+// IMPLEMENTED BY : Nick
 //*********************************************************************
-void MergeSort(int unorderedList[], int lowIdx, int highIdx)
+void MergeSort (int unorderedList[], int lowIdx, int highIdx )
 {
   int midIdx = 0;
   
@@ -231,20 +254,20 @@ void MergeSort(int unorderedList[], int lowIdx, int highIdx)
 }
 
 //*********************************************************************
-// FUNCTION		: MergeList
-// DESCRIPTION	: Merges, in order, two sections of a list created
-//			  by merge sort.
+// FUNCTION       : MergeList
+// DESCRIPTION    : Merges, in order, two sections of a list created
+//                  by merge sort.
 // INPUT
-//	Parameters	: lowIdx		- low index of range in list to merge
-//			  midIdx		- middle index of range “ “ “ “
-//			  highIdx		- high index of range “ “ “ “
+//	Parameters    : lowIdx		- low index of range in list to merge
+//                  midIdx		- middle index of range “ “ “ “
+//                  highIdx		- high index of range “ “ “ “
 // OUTPUT   
 //  	Parameters	: unorderedList[] 	- array fully or partially ordered numbers
-//	Return	: void
-// CALLS TO		: 
-// IMPLEMENTED BY	: Nick
+//    Return      : void
+// CALLS TO       : none
+// IMPLEMENTED BY : Nick
 //*********************************************************************
-void MergeList(int unorderedList[], int lowIdx, int midIdx, int highIdx)
+void MergeList( int unorderedList[], int lowIdx, int midIdx, int highIdx )
 {
   int temp[MAX_ARRAY_SIZE];  
   int leftIdx = lowIdx;
@@ -298,7 +321,7 @@ void MergeList(int unorderedList[], int lowIdx, int midIdx, int highIdx)
 // CALLS TO		: 
 // IMPLEMENTED BY	: 
 //*********************************************************************
-double PerformQuickSort( int unorderedIntegers[], int sortedIntegers[] )
+void PerformQuickSort( int unorderedIntegers[] )
 {
 }
 
@@ -309,12 +332,12 @@ double PerformQuickSort( int unorderedIntegers[], int sortedIntegers[] )
 //	Parameters	: lowIdx		- low index of range in list to merge
 //			  highIdx		- high index of range “ “ “ “
 // OUTPUT   
-//  	Parameters	: unorderedList[] 	- array fully or partially ordered numbers
+//  	Parameters	: unorderedIntegers[] 	- array fully or partially ordered numbers
 //	Return	: void
 // CALLS TO		: PerformInsertionSort
 // IMPLEMENTED BY	: Nick
 //*********************************************************************
-void QuickSort(int unorderedList[], int lowIdx, int highIdx)
+void QuickSort( int unorderedIntegers[], int lowIdx, int highIdx )
 {
 }
 
@@ -345,6 +368,6 @@ bool VerifySortedArray( int sortedIntegers[] )
 // CALLS TO		: none
 // IMPLEMENTED BY	: 
 //*********************************************************************
-void DisplayResultAverage( double **sortTimesByType[] )
+void DisplayResultAverage( int **sortTimesByType )
 {
 }
